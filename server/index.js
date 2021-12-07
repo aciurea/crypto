c571565dceec21948787fa5b60b140c000373525
alin.ciurea <alin.ciurea@siiromania.ro>
2021-12-07 20:07:35 +0200

    Watch for file changes and read the file, send email and remove the file.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,10 @@
 const nodemailer = require('nodemailer');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const axios = require('axios').default;
+const fs = require('fs');
 require('dotenv').config();
 
-const APP_URL = process.env.APP_URL;
-const APP_PORT = process.env.APP_PORT;
 const FROM = process.env.FROM;
 const TO = process.env.TO;
 const PASSWORD = process.env.PASSWORD;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.options('*', cors());
-app.use(cors());
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -27,38 +16,43 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendUrl = `/sendReport`;
-app.listen(APP_PORT, () => {
-  // transporter.verify().then(console.log).catch(console.error);
-});
+const folderName = `${__dirname}/downloads`;
+function watch() {
+  fs.watch(folderName, (eventType, filename) => {
+    if (eventType === 'rename') {
+      const filePath = `${folderName}/data.json`;
+      fs.readFile(filePath, function (err, fileToRead) {
+        if (!err) {
+          const { negative, positive } = buildEmail(JSON.parse(fileToRead));
 
-console.log(`API Gateway listening ${APP_URL} at:${APP_PORT} --> ${sendUrl}`);
+          const mailOptions = {
+            from: FROM,
+            to: TO,
+            subject: 'Crypto updates',
+            text: `${negative}
+           --------------------------------------------------------------------------
+            ${positive}
+            `,
+          };
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
 
-app.post(sendUrl, async (req, res) => {
-  const report = req.body.report;
-  const { negative, positive } = buildEmail(report);
-  const mailOptions = {
-    from: FROM,
-    to: TO,
-    subject: 'Crypto updates',
-    text: `NEGATIVE: ${negative}
-    
-    POSITIVE: ${positive}
-    `,
-  };
+          fs.unlinkSync(filePath);
+        } else {
+          console.error('there is an error', err);
+        }
+      });
+    }
+    console.log(eventType, filename);
+  });
+}
 
-  // transporter.sendMail(mailOptions, function (error, info) {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('Email sent: ' + info.response);
-  //   }
-  // });
-
-  try {
-    res.json({ success: false });
-  } catch (error) {}
-});
+watch();
 
 function buildEmail(report) {
   const lastDayColTxt = '24h %';
@@ -74,55 +68,32 @@ function buildEmail(report) {
   const negative =
     report.negative?.reduce((acc, next) => {
       const [name] = Object.keys(next);
-      acc += `\n
-  ${name} => ${next.link} lastDay: ${next[lastDayColTxt]} lastWeek: ${next[last7DayColTxt]}
+      const obj = next[name];
+
+      acc += `
+      NAME: ${name}
+      LINK: ${obj?.link}
+      lastDay: ${obj?.[lastDayColTxt]}
+      lastWeek: ${obj?.[last7DayColTxt]}
   `;
 
       return acc;
-    }, '') || '';
+    }, 'NEGATIVE ==> ') || '';
 
   const positive =
     report.positive?.reduce((acc, next) => {
       const [name] = Object.keys(next);
-      acc += `\n
-  ${name} => ${next.link} lastDay: ${next[lastDayColTxt]} lastWeek: ${next[last7DayColTxt]}
+      const obj = next[name];
+
+      acc += `
+      NAME: ${name}
+      LINK: ${obj?.link}
+      lastDay: ${obj?.[lastDayColTxt]}
+      lastWeek: ${obj?.[last7DayColTxt]}
   `;
 
       return acc;
-    }, '') || '';
+    }, 'POSITIVE ==>') || '';
 
   return { negative, positive };
 }
-// const start = `/start`;
-// app.get(start, async (req, res) => {
-//   axios
-//     .get('https://coinmarketcap.com/?page=1')
-//     .then(({ data }) => {
-//       const [table] = getElementByTag(data, 'table', true);
-
-//       return res.json({ data: table });
-//     })
-//     .catch((error) => {
-//       res.status(500).json({ error });
-//     });
-// });
-
-// function getElementByTag(string, tag, hasAttributes = false) {
-//   const firstTag = hasAttributes ? `<${tag} ` : `<${tag}>`;
-//   const endTag = `</${tag}>`;
-//   const firstIndex = string.indexOf(firstTag);
-//   const endIndex = string.indexOf(endTag);
-
-//   const element = string.substring(firstIndex, endIndex + endTag.length);
-//   return [element, string.substring(endIndex + endTag.length)];
-// }
-
-// function findAll(string, tag, hasAttributes = false) {
-//   const arr = [];
-//   const [element, rest] = getElementByTag(string, tag, hasAttributes);
-//   if (element) {
-//     arr.push(element, ...findAll(rest, tag, hasAttributes));
-//   }
-
-//   return arr;
-// }
